938623d5f7c9bf2cd5e67817da6af500a87bdd18
Julian <julianrosenthal@yahoo.com>
2021-04-06 14:38:04 -0700

    presentational component for pic

--- src/components/Pic.js
+++ src/components/Pic.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 
+
 export class Pic extends Component {
+  
+  
+  
   render() {
+    const {imgUrl, author} = this.props
     return (
       <div>
-        
+        <img src={imgUrl} alt="random"/>
+        <p>Photo By: {author}</p>
       </div>
     )
   }
 }
 
 export default Pic
+
