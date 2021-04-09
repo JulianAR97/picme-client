7960d18c2a6369c8ae86ad4bcc4fb80f74587fbd
Julian <julianrosenthal@yahoo.com>
2021-04-08 17:56:23 -0700

    added loading wheel from material-ui

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 
 const Loading = () => {
   return(
     <>
-      Loading...
+      <CircularProgress color="secondary"/>
     </>
   )
 }
