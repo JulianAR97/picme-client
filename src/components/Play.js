121c3330f111d50d2b22f20d134f85616c129c53
Julian <julianrosenthal@yahoo.com>
2021-04-09 19:47:05 -0700

    removed logic from Play component

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,63 +1,21 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { getPic, addPic, updatePic } from '../actions/picActions'
 import PicContainer from '../containers/PicContainer'
-import Button from './Button'
-import Loading from './Loading'
+
 
 export class Play extends Component {
 
-  componentDidMount() {
-    this.props.getPic()
-    // if (this.props.pics.find(p => p.id === this.props.pic.id)) {
-    //   this.setState({
-    //     existingPic: true
-    //   })
-    // }
-  }
-
-  handleClick = (e) => {
-    let pic = this.props.pics.find(p => p.id === Number(this.props.pic.id))
-    
-    if (e.target.name === 'upvote') {
-      if (pic) {
-        this.props.updatePic({...pic, likes: pic.likes + 1})
-      } else {
-        this.props.addPic({...this.props.pic, likes: this.props.pic.likes + 1})
-      }
-    } else {
-      if (pic) {
-        console.log('hit update:', pic)
-        this.props.updatePic({...pic, likes: pic.likes - 1})
-      }
-    }
-    this.props.getPic()
-  }
   
   render() {
-    if (this.props.picLoading) {
-      return <Loading />
-    } else {
+     
+    return (
       
-      const {pic} = this.props
-      
-      return (
-        
-        <div>
-          <PicContainer url={pic.url} author={pic.author} />
-          <Button name="downvote" handleClick={this.handleClick} text="minus"/>
-          <Button name="upvote" handleClick={this.handleClick} text="check"/>
-        </div>
-      
-      )
-    }
+      <div>
+        <PicContainer />
+      </div>
+    
+    )
   }
+  
 }
 
-const mapStateToProps = state => ({
-  pic: state.pic,
-  pics: state.pics,
-  picLoading: state.picLoading,
-})
-
-export default connect(mapStateToProps, {getPic, addPic, updatePic})(Play)
+export default Play
