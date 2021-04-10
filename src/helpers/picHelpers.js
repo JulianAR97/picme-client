9bda7960b818f1ee0237ef2b22217067c2fe1522
Julian <julianrosenthal@yahoo.com>
2021-04-09 19:47:38 -0700

    added remove highlight function to picHelpers

--- src/helpers/picHelpers.js
+++ src/helpers/picHelpers.js
@@ -1,3 +1,11 @@
 export const picExists = (pics, pic) => {
   return !!pics.find(p => p.id === Number(pic.id))
 }
+
+// Removes highlighting from sides of picture frame
+export const removeHighlight = () => {
+  let subContainers = document.getElementsByClassName('subContainer');
+  for (let sC of subContainers) {
+    sC.classList.remove('highlightGreen', 'highlightRed')
+  }
+}
