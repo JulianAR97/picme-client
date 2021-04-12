74fefa31911320725ccc13904ceaaee4a7d1a3f1
Julian <julianrosenthal@yahoo.com>
2021-04-11 19:50:13 -0700

    added creation and setting of cookies to App component mounting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Loading from './components/Loading'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux'
-import { getPics } from './actions/picActions';
+import { getPics, setCookie, getMyCollection } from './actions/picActions';
 import PicCollection from './containers/PicCollection'
 import About from './components/About';
 import Play from './components/Play'
@@ -11,40 +11,50 @@ import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import { ThemeProvider } from '@material-ui/core/styles'
 import mainTheme from './styles/theming'
+import {createCookie} from './helpers/appHelpers'
 
 
 class App extends Component {
 
   componentDidMount() {
     this.props.getPics()
+    // We create a cookie id so that we can remember users and they can save pics without
+    // logging in.  Similar to a shopping cart. 
+    // cookie is in form "userUUID=random_id"
+    const userUUID = createCookie().split('=')[1] // we just want the random id
+    this.props.setCookie(userUUID)
+
+
   }
 
   render() {
-    if (this.props.picsLoading) {
+    if (this.props.loading) {
       return <Loading />
+    } else {
+      return (
+        <ThemeProvider theme={mainTheme}>
+  
+          
+       
+  
+            <Router>
+              <NavBar />
+              <Switch>
+                  <Route exact path="/" component={ About } />
+                  <Route exact path="/play" component={ Play } />
+                  <Route exact path="/hottest" component={ PicCollection } />
+                  <Route component={ErrorPage} />
+              </Switch>
+            </Router>
+       
+            <Footer />
+          
+        </ThemeProvider>
+      );
+      
     }
 
 
-    return (
-      <ThemeProvider theme={mainTheme}>
-
-        
-     
-
-          <Router>
-            <NavBar />
-            <Switch>
-                <Route exact path="/" component={ About } />
-                <Route exact path="/play" component={ Play } />
-                <Route exact path="/hottest" component={ PicCollection } />
-                <Route component={ErrorPage} />
-            </Switch>
-          </Router>
-     
-          <Footer />
-        
-      </ThemeProvider>
-    );
     
 
 
@@ -56,4 +66,4 @@ const mapStateToProps = state =>  ({
 })
 
 
-export default connect(mapStateToProps, {getPics})(App);
+export default connect(mapStateToProps, {getPics, setCookie, getMyCollection})(App);
